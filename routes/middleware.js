7ed6bfa2ec7b1c2224de232c5ee2ef0b8c87a4c1
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-28 13:20:12 +0700

    update: helmet

--- routes/middleware.js
+++ routes/middleware.js
@@ -8,11 +8,17 @@ const path = require('path');
 
 module.exports = (app) => {
 
-    app.use(cors({ optionsSuccessStatus: 200 }));
+    app.use(cors({ origin: '*' }));
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(helmet());
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'"],
+            styleSrc: ["'self'"]
+        }
+    }))
 
     app.use(express.static(path.join(__dirname, '../public')));
 
