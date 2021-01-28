31c89491033fdb8d61b5e8e8993e11584f80e0d5
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-01-28 12:39:43 +0700

    update: functional test

--- test/functional.js
+++ test/functional.js
@@ -4,45 +4,97 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 
 const assert = chai.assert;
-const server = 'https://localhost:3000';
+const server = 'http://localhost:3000';
 chai.use(chaiHttp);
 
-describe('functional tests', () => {
+describe('Functional tests', () => {
 
-    it('test1', (done) => {
-        chai.request(server)
-            .get('/api')
-            .query({ sample: 'sample1' })
-            .end((err, res) => {
-                assert.equal(res.status, 200);
-                done();
-            })
+    describe('Viewing one stock', () => {
+        it('GET request to /api/stock-prices', (done) => {
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog' })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.isObject(res.body.stockData);
+                    assert.isString(res.body.stockData.stock);
+                    assert.isNumber(res.body.stockData.price);
+                    assert.isNumber(res.body.stockData.likes);
+                    done();
+                })
+        })
     })
 
-    it('test2', (done) => {
-        chai.request(server)
-            .post('/api')
-            .send({ sample: 'sample1' })
-            .end((err, res) => {
-                assert.equal(res.status, 200);
-                done();
-            })
+    describe('Viewing one stock and liking it', () => {
+        it('GET request to /api/stock-prices', (done) => {
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: 'goog', like: 'true' })
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.isObject(res.body.stockData);
+                    assert.isString(res.body.stockData.stock);
+                    assert.isNumber(res.body.stockData.price);
+                    assert.isNumber(res.body.stockData.likes);
+                    done();
+                })
+        })
     })
 
-    it('test1', (done) => {
-        chai.request(server)
-            .post('/api')
-            .send({ sample: 'sample1' })
-            .then(data => {
-                chai.request(server)
-                    .put('/api')
-                    .send({ id: data.id })
-                    .end((err, res) => {
-                        assert.equal(res.status, 200);
-                        done();
-                    })
-            })
-            .catch(err => done(err));
+    describe('Viewing the same stock and liking it again', () => {
+        it('GET request to /api/stock-prices', (done) => {
+            let random = Date.now();
+            chai.request(server)
+                .get('/api/stock-prices')
+                .query({ stock: random })
+                .then(res => {
+                    let like_before = res.body.stockData.likes;
+                    chai.request(server)
+                        .get('/api/stock-prices')
+                        .query({ stock: random, like: true })
+                        .end((err, res) => {
+                            let like_after = res.body.stockData.likes;
+                            assert.equal(like_before, like_after - 1);
+                            done();
+                        })
+                })
+                .catch(err => done(err))
+        })
     })
 
+    describe('Viewing two stocks', () => {
+        it('GET request to /api/stock-prices', (done) => {
+            chai.request(server)
+                .get('/api/stock-prices?stock=goog&stock=msft')
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.isArray(res.body.stockData);
+                    assert.isString(res.body.stockData[0].stock);
+                    assert.isNumber(res.body.stockData[0].price);
+                    assert.isNumber(res.body.stockData[0].rel_likes);
+                    assert.isString(res.body.stockData[1].stock);
+                    assert.isNumber(res.body.stockData[1].price);
+                    assert.isNumber(res.body.stockData[1].rel_likes);
+                    done();
+                })
+        })
+    })
+
+    describe('Viewing two stocks and liking them', () => {
+        it('GET request to /api/stock-prices', (done) => {
+            chai.request(server)
+                .get('/api/stock-prices?stock=goog&stock=msft&like=true')
+                .end((err, res) => {
+                    assert.equal(res.status, 200);
+                    assert.isArray(res.body.stockData);
+                    assert.isString(res.body.stockData[0].stock);
+                    assert.isNumber(res.body.stockData[0].price);
+                    assert.isNumber(res.body.stockData[0].rel_likes);
+                    assert.isString(res.body.stockData[1].stock);
+                    assert.isNumber(res.body.stockData[1].price);
+                    assert.isNumber(res.body.stockData[1].rel_likes);
+                    done();
+                })
+        })
+    })
 })
